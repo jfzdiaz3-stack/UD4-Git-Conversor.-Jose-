4eb8a5677274599073c87c2d456185faa3bf34f2
jfzdiaz3-stack <jfzdiaz.3@gmail.com>
2025-12-03 16:57:14 +0100

    Añadido comentario de colaborador Juan Pérez

--- src/conversorUnidades/java/ConversorUnidades.java
+++ src/conversorUnidades/java/ConversorUnidades.java
@@ -2,7 +2,8 @@ package conversorUnidades.java;
 
 public class ConversorUnidades {
 
-	
+	// Contribución de Juan Pérez: Módulo de temperatura.
+
 		// TODO Auto-generated method stub
 		public static double celsiusAFahrenheit(double celsius) {
 		    return (celsius * 9/5) + 32;
